b89d3ffede2a1d592930157abc789a3baacc9236
Moises Castro <mcastro60@gatech.edu>
2025-08-26 12:58:29 -0700

    Added Coroutine for status effect on bing bong

--- src/PlushieParty/Patches/CharacterItemsPatch.cs
+++ src/PlushieParty/Patches/CharacterItemsPatch.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace PlushieParty.Patches
 {
@@ -28,16 +29,82 @@ namespace PlushieParty.Patches
             if ((now.itemTags & Item.ItemTags.BingBong) != 0)
             {
                 Plugin.Log.LogInfo("BingBong is Held");
-
-                GUIManager.instance.AddStatusFX(CharacterAfflictions.STATUSTYPE.Poison, 14f);
-                character.refs.afflictions.AddStatus(CharacterAfflictions.STATUSTYPE.Poison, 0.5f);
             }    
 
         }
 
+        public static bool inCoroutine = false;
 
-    
 
+        [HarmonyPatch(typeof(CharacterItems), "FixedUpdate")]
+        [HarmonyPostfix]
+        private static void FixedUpdatePostFix(CharacterItems __instance)
+        {
+            Character character = __instance.GetComponentInParent<Character>();
+            Item now = character.data.currentItem;
+
+            if (now == null)
+            {
+                return;
+            }
+
+            if (!inCoroutine && (now.itemTags & Item.ItemTags.BingBong) != 0)
+            {
+                __instance.StartCoroutine(IPlushieCooldown(character));
+            }
+
+        }
+
+        private static IEnumerator IPlushieCooldown(Character character)
+        {
+            inCoroutine = true;
+            Plugin.Log.LogInfo("Poison Coroutine Started");
+            yield return new WaitForSeconds(1f);
+            Plugin.Log.LogInfo("Poison Coroutine Finished");
+            character.refs.afflictions.SubtractStatus(CharacterAfflictions.STATUSTYPE.Poison, 0.1f);
+            inCoroutine = false;
+        }
+
+
+
+
+
+        [HarmonyPatch(typeof(GUIManager), "LateUpdate")]
+        [HarmonyPostfix]
+        private static void LateUpdatePostFix(GUIManager __instance)
+        {
+            if (InputSystem.actions.FindAction($"Hotbar{5}").WasPressedThisFrame())
+            {
+                Plugin.Log.LogInfo("Hotbar 5 is being Pressed in GUIManager");
+
+                __instance.AddStatusFX(CharacterAfflictions.STATUSTYPE.Poison, 14f);
+            }
+
+
+        }
+
+
+        [HarmonyPatch(typeof(Character), "Update")]
+        [HarmonyPostfix]
+        private static void UpdatePostFix(Character __instance)
+        {
+           
+            if (__instance == null)
+            {
+                Plugin.Log.LogInfo("Character is null in Character");
+                return;
+            }
+
+            if (InputSystem.actions.FindAction($"Hotbar{5}").WasPressedThisFrame())
+            {
+                Plugin.Log.LogInfo("Hotbar 5 is being Pressed in Character");
+
+                __instance.refs.afflictions.AddStatus(CharacterAfflictions.STATUSTYPE.Poison, 0.5f);
+
+            }
+
+
+        }
 
 
     }
